6eeab63f7d4df6dd0ed1773386b4353d3ad1a4cc
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-18 14:47:06 +0300

    lisättiin tehtävä 3.1

---
+++ Teht_2.4.py
@@ -0,0 +1,9 @@
+# Kirjoita ohjelma, joka kysyy kolme kokonaislukua.
+# Ohjelma tulostaa lukujen summan, tulon ja keskiarvon.
+
+luku1 = int(input("Anna ensimmäinen luku: "))
+luku2 = int(input("Anna toinen luku: "))
+luku3 = int(input("Anna kolmas luku: "))
+
+print(f"Lukujen summa on {luku1+luku2+luku3}")
+print(f"Lukujen tulo on {luku1*luku2*luku3}")
---
+++ Teht_3.1.py
@@ -0,0 +1,14 @@
+# Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä.
+# Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen
+# ilmoittaen samalla käyttäjälle, montako senttiä alimmasta sallitusta
+# pyyntimitasta puuttuu.
+# Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+print("Minkäs kokisen kuhan sait?")
+pituus = float(input("Kerro kuhan pituus senttimetreissä: "))
+
+if pituus < 37:
+    erotus = 37 - pituus
+    print(f"Heitä se takaisin järveen, se on {erotus:.2f}cm alle pyyntimitan.")
+else:
+    print("Onneksi olkoon, onpa vonkale!")
