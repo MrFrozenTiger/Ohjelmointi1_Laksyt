64f70abb575b4af7f65facd3482c281438ee0295
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-19 16:14:32 +0300

    Sievistettiin tehtävää 3.4

---
+++ Teht_3.4.py
@@ -0,0 +1,21 @@
+# Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
+# Vuosi on karkausvuosi, jos se on jaollinen neljällä. Sadalla jaolliset vuodet
+# ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+
+while True:
+    vuosi = input("Syötä vuosi: ")
+    try:
+        vuosiluku = int(vuosi)
+        break
+    except ValueError:
+        print("Virhe.")
+
+if vuosiluku % 100 == 0:
+    if vuosiluku % 400 == 0:
+        print(f"Vuosi {vuosiluku} on karkausvuosi.")
+    else:
+        print(f"Vuosi {vuosiluku} ei ole karkausvuosi.")
+elif vuosiluku % 4 == 0:
+    print(f"Vuosi {vuosiluku} on karkausvuosi.")
+else:
+    print(f"Vuosi {vuosiluku} ei ole karkausvuosi.")
