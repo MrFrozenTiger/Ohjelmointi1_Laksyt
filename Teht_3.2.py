99230be41b9068b13a065c5534a3ea9dbd2855a2
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-19 10:00:04 +0300

    lisättiin tehtävä 3.2

---
+++ Teht_3.2.py
@@ -0,0 +1,24 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C)
+# ja tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti.
+# Tehtävässä on käytettävä if/elif/else-toistorakennetta.
+
+# LUX on parvekkeellinen hytti yläkannella.
+# A on ikkunallinen hytti autokannen yläpuolella.
+# B on ikkunaton hytti autokannen yläpuolella.
+# C on ikkunaton hytti autokannen alapuolella.
+
+# Jos käyttäjä syöttää kelvottoman hyttiluokan,
+# ohjelma tulostaa Virheellinen hyttiluokka.
+
+syote = input("Syötä hyttiluokka: ").upper()
+
+if syote == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif syote == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif syote == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif syote == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("ERROR. Virheellinen hyttiluokka.")
