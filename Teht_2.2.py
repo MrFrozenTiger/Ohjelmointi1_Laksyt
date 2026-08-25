4f06717e4731635d071f930e5bd5e9033e311831
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-25 21:31:24 +0300

    Poistettiin turhat kopiot.

--- Teht_2.2.py
+++
@@ -1,6 +0,0 @@
-#Kirjoita ohjelma, joka kysyy ympyrän säteen ja tulostaa sen pinta-alan.
-#pii * r ^2
-import math
-sade = float(input("Syötä ympyrän säde: "))
-p_ala = math.pi * sade ** 2
-print(f"{p_ala:.3f}")
