d22a4ebcd9835ed200783d9da00405e64ca93705
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-26 13:29:53 +0300

    Tehtävä 4.3 valmis.

--- Teht_4/Teht_4.3.py
+++ Teht_4/Teht_4.3.py
@@ -2,9 +2,22 @@
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 
-syote = input("Syötä luku: ")
-suurin = 0
-pienin = 0
-while float(syote) ==
+suurin = ""
+pienin = ""
+while True:
+    luku = input("Syötä luku (tyhjä lopettaa): ")
+    if luku == "":
+        break
+    try:
+        luku = int(luku)
+        if suurin == "" or pienin == "":
+            suurin = luku
+            pienin = luku
+        elif int(luku) > suurin:
+            suurin = int(luku)
+        elif int(luku) < pienin:
+            pienin = int(luku)
+    except ValueError:
+        print("Virhe. Antamasi syöte ei ole luku.")
 
 print(f"Suurin luku: {suurin}, pienin luku: {pienin}")
