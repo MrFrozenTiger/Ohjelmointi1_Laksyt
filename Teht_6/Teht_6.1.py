f8735c063f17effd5816c5d708a47969c4a58061
MrFrozenTiger <juho.saniola@gmail.com>
2026-08-31 10:15:37 +0300

    lisättiin tehtävä 6.1.

--- Teht_6/Teht_6.1.py
+++ Teht_6/Teht_6.1.py
@@ -2,3 +2,14 @@
 # väliltä 1..6. Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
 
+import random
+
+def arpa ():
+    luku = random.randint(1,6)
+    return luku
+luku = 0
+heitot = 0
+while luku != 6:
+    luku = arpa()
+    heitot += 1
+    print(f"{heitot}. heiton silmäluku on {luku}")
