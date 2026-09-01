41852ff039609f6c1a25c0b3cc9fdef517ecae8d
MrFrozenTiger <juho.saniola@gmail.com>
2026-09-01 16:28:32 +0300

    lisättiin tehtävä 6.2.

---
+++ Teht_6/Teht_6.2.py
@@ -0,0 +1,24 @@
+# Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan tahkojen yhteismäärän.
+# Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
+# Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan
+# nopan maksimisilmäluku, joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+import random
+
+def arpa (min, max):
+    luku = random.randint(min, max)
+    return luku
+
+luku = 0
+heitot = 0
+while True:
+    noppa = input("Valitse nopan koko: ").lower().strip("d")
+    try:
+        noppa = int(noppa)
+        break
+    except:
+        print("Virhe.")
+while luku != noppa:
+    luku = arpa(1, noppa)
+    heitot += 1
+    print(f"{heitot}. heiton silmäluku on {luku}")
