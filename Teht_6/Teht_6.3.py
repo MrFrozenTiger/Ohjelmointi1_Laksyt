e8069d7cc70387bdfd30f95c9aeb4c5580c8fca0
MrFrozenTiger <juho.saniola@gmail.com>
2026-09-01 17:51:30 +0300

    Lisättiin tehtävä 6.4.

--- Teht_6/Teht_6.3.py
+++ Teht_6/Teht_6.3.py
@@ -5,17 +5,28 @@
 # kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
 
-def gallonat_litroiksi():
+def gallonat_litroiksi(gallonat):
     litrat = gallonat * 3.785
     return litrat
 
-while True:
-    gallonat = input("Syötä gallonat: ")
-    try:
-        gallonat = float(gallonat)
-        break
-    except ValueError:
-        print("Virheellinen syöte.")
-litrat = gallonat_litroiksi()
-print(f"{litrat:.3f}")
+gallonat = float(input("Syötä gallonat (negatiivinen lopettaa): "))
+while gallonat >= 0:
+    litrat = gallonat_litroiksi(gallonat)
+    print(f"{gallonat} gallonaa on {litrat} litraa.")
+    gallonat = float(input("Syötä gallonat (negatiivinen lopettaa): "))
+print("Ohjelma päättyy.")
 
+#gallonat = 0
+#litrat = 0
+#while True:
+#    gallonat = input("Syötä gallonat (negatiivinen lopettaa): ")
+#    try:
+#        gallonat = float(gallonat)
+#        if gallonat < 0:
+#            break
+#        litrat = gallonat_litroiksi(gallonat)
+#        print(f"{gallonat} gallonaa on {litrat:.3f} litraa.")
+#    except ValueError:
+#        print("Virheellinen syöte.")
+#        gallonat = 0
+#print("Ohjelma päättyy.")
---
+++ Teht_6/Teht_6.4.py
@@ -0,0 +1,22 @@
+# Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
+# Ohjelma palauttaa listassa olevien lukujen summan.
+# Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota
+# ja tulostat sen palauttaman summan.
+
+def lukujen_summa(x):
+    summa = 0
+    for i in lista:
+        summa += int(i)
+    return summa
+
+lista = []
+luku = input("Lisää luku (tyhjä lopettaa): ")
+while luku != "":
+    try:
+        luku = int(luku)
+        lista.append(luku)
+    except ValueError:
+        print("Virheellinen syöte.")
+    luku = input("Lisää luku (tyhjä lopettaa): ")
+summa = lukujen_summa(lista)
+print("Lukujen summa on ", summa)
