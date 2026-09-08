160a1159170c594dcd2c471f899507f758c04454
MrFrozenTiger <juho.saniola@gmail.com>
2026-09-08 13:26:28 +0300

    Lisättiin tehtävä 8.2.

---
+++ Teht_8/Teht_8.2.py
@@ -0,0 +1,27 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa
+# kyseisessä maassa olevien lentokenttien lukumäärät tyypeittäin.
+# Esimerkiksi Suomen osalta tuloksena on saatava tieto siitä, että pieniä lentokenttiä
+# on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
+
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host="127.0.0.1",
+         port= 3306,
+         database="flight_game",
+         user="root",
+         password="2511",
+         autocommit=True
+         )
+
+
+koodi = input("Syötä maa koodi: ").upper()
+sql = f"select type, count(*) from airport where iso_country = '{koodi}' group by type;"
+kursori = yhteys.cursor()
+kursori.execute(sql)
+
+tulos = kursori.fetchall()
+if tulos != []:
+    print(tulos)
+else:
+    print("Virheellinen syöte.")
